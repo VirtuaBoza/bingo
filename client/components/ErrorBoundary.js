2c656335cfbbc0e523a9f664e5b58189f3f45a93
VirtuaBoza <andrew.m.boza@gmail.com>
2020-03-02 20:14:25 -0500

    moves rn app to client folder

---
+++ client/components/ErrorBoundary.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <View>
+          <Text>Something went wrong.</Text>
+        </View>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
---
+++ client/screens/NewGameScreen.js
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+function Item({ text }) {
+  return (
+    <View style={styles.item}>
+      <Text>{text}</Text>
+    </View>
+  );
+}
+
+function ItemSeparator() {
+  return <View style={styles.itemSeparator} />;
+}
+
+function TileSelector({ filename }) {
+  return (
+    <TouchableOpacity>
+      {filename === '3x3wfree' ? (
+        <Image
+          source={require('../assets/images/3x3wfree.png')}
+          style={styles.tileImage}
+        />
+      ) : filename === '3x3' ? (
+        <Image
+          source={require('../assets/images/3x3.png')}
+          style={styles.tileImage}
+        />
+      ) : filename === '4x4' ? (
+        <Image
+          source={require('../assets/images/4x4.png')}
+          style={styles.tileImage}
+        />
+      ) : filename === '5x5wfree' ? (
+        <Image
+          source={require('../assets/images/5x5wfree.png')}
+          style={styles.tileImage}
+        />
+      ) : filename === '5x5' ? (
+        <Image
+          source={require('../assets/images/5x5.png')}
+          style={styles.tileImage}
+        />
+      ) : null}
+    </TouchableOpacity>
+  );
+}
+
+export default function NewGameScreen() {
+  const [value, setValue] = React.useState('');
+  const [items, setItems] = React.useState([]);
+  const ref = React.useRef();
+
+  function handleSubmitValue() {
+    if (value.trim() && !items.find(i => i === value)) {
+      setItems(i => [...i, value.trim()]);
+    }
+    setValue('');
+    setTimeout(() => {
+      ref.current.focus();
+    }, 1);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.tileSelectContainer}>
+        <ScrollView horizontal={true}>
+          {items.length >= 8 && <TileSelector filename="3x3wfree" />}
+          {items.length >= 9 && <TileSelector filename="3x3" />}
+          {items.length >= 16 && <TileSelector filename="4x4" />}
+          {items.length >= 24 && <TileSelector filename="5x5wfree" />}
+          {items.length >= 25 && <TileSelector filename="5x5" />}
+        </ScrollView>
+      </View>
+      <TextInput
+        style={styles.item}
+        value={value}
+        onChangeText={text => setValue(text)}
+        onSubmitEditing={handleSubmitValue}
+        ref={ref}
+        autoFocus
+      />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <Item text={item} />}
+        keyExtractor={item => item}
+        ItemSeparatorComponent={ItemSeparator}
+      />
+    </View>
+  );
+}
+
+NewGameScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  item: {
+    padding: 20,
+  },
+  itemSeparator: {
+    marginLeft: 10,
+    marginRight: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  editItem: {
+    backgroundColor: 'lightgray',
+    padding: 20,
+  },
+  tileImage: {
+    height: 50,
+    width: 50,
+  },
+  tileSelectContainer: {
+    height: 60,
+  },
+  debug1: {
+    backgroundColor: 'red',
+  },
+  debug2: {
+    backgroundColor: 'blue',
+  },
+});
